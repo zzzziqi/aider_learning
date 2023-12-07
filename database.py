ac55f175955a44b688260c849628f7b1cfea5c90
zzzziqi <zzeng002@gold.ac.uk>
2023-12-07 15:13:25 +0800

    aider: Added function to create a new table for users in the SQLite database.
    # Aider chat conversation:
    
    USER: I dont' thnk I have the db.squlite, please help me to add one
    
    ASSISTANT: To create a new SQLite database, you need to modify the `database.py` file to include a function that creates a new table for users when the database is created.
    
    Here are the steps:
    
    1. Add a new function `create_users_table()` that creates a new table named `users` with columns `username` and `password`.
    2. Call `create_users_table()` in `create_connection()`.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    database.py
    ```python
    <<<<<<< SEARCH
    def create_connection():
        conn = None;
        try:
            conn = sqlite3.connect('db.sqlite') # create a database connection to a SQLite database
            print(sqlite3.version)
        except Error as e:
            print(e)
        finally:
            if conn:
                conn.close()
    =======
    def create_users_table():
        conn = sqlite3.connect('db.sqlite')
        cur = conn.cursor()
        cur.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)")
        conn.commit()
        conn.close()
    
    def create_connection():
        conn = None;
        try:
            conn = sqlite3.connect('db.sqlite') # create a database connection to a SQLite database
            print(sqlite3.version)
            create_users_table()
        except Error as e:
            print(e)
        finally:
            if conn:
                conn.close()
    >>>>>>> REPLACE
    ```
    
    After these changes, when you run your application and `create_connection()` is called, a new SQLite database `db.sqlite` will be created if it doesn't exist, and a new table `users` will be created in the database.

--- database.py
+++ database.py
@@ -1,11 +1,19 @@
 import sqlite3
 from sqlite3 import Error
 
+def create_users_table():
+    conn = sqlite3.connect('db.sqlite')
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)")
+    conn.commit()
+    conn.close()
+
 def create_connection():
     conn = None;
     try:
         conn = sqlite3.connect('db.sqlite') # create a database connection to a SQLite database
         print(sqlite3.version)
+        create_users_table()
     except Error as e:
         print(e)
     finally:
