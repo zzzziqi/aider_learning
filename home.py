d5e4d8003ca4d2ce253614e6aa26639e1c119b44
zzzziqi <zzeng002@gold.ac.uk>
2023-12-07 14:48:44 +0800

    aider: Created a home page that includes the login and register pages.
    # Aider chat conversation:
    
    USER: I want a home page to include all the page including login and register
    
    ASSISTANT: To create a home page that includes the login and register pages, we need to modify `home.py`:
    
    1. Import the necessary Streamlit package.
    2. Create a Streamlit application with a sidebar for navigation between the home, login, and register pages.
    
    Here is the *SEARCH/REPLACE* block:
    
    home.py
    ```python
    <<<<<<< SEARCH
    =======
    import streamlit as st
    from login import main as login_main
    from register import main as register_main
    
    def main():
        st.title("Home Page")
    
        menu = ["Home", "Login", "Register"]
        choice = st.sidebar.selectbox("Menu", menu)
    
        if choice == "Home":
            st.subheader("Home")
        elif choice == "Login":
            login_main()
        elif choice == "Register":
            register_main()
    
    if __name__ == "__main__":
        main()
    >>>>>>> REPLACE
    ```
    
    This code will create a Streamlit application with a title "Home Page". It will have a sidebar with a selectbox for navigation between the home, login, and register pages. Depending on the selected option, it will display the corresponding page. The login and register pages are imported from the `login.py` and `register.py` files respectively.

---
+++ home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from login import main as login_main
+from register import main as register_main
+
+def main():
+    st.title("Home Page")
+
+    menu = ["Home", "Login", "Register"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    if choice == "Home":
+        st.subheader("Home")
+    elif choice == "Login":
+        login_main()
+    elif choice == "Register":
+        register_main()
+
+if __name__ == "__main__":
+    main()
