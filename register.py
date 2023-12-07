85775c94146cac0f62461a75eb950a335cebc44b
zzzziqi <zzeng002@gold.ac.uk>
2023-12-07 14:47:21 +0800

    aider: Created a registration page with fields for username, email, password, and password confirmation, and a "Register" button.
    # Aider chat conversation:
    
    USER: I also want to create another register page for user to allow new users to sign up with fields for username, email, password, and password confirmation.
    
    ASSISTANT: To create a registration page using Streamlit, we need to modify `register.py`:
    
    1. Import the necessary Streamlit package.
    2. Create a Streamlit application with username, email, password, and password confirmation fields, and a "Register" button.
    
    Here is the *SEARCH/REPLACE* block:
    
    register.py
    ```python
    <<<<<<< SEARCH
    =======
    import streamlit as st
    
    def main():
        st.title("Registration Page")
        username = st.text_input("Username")
        email = st.text_input("Email")
        password = st.text_input("Password", type='password')
        confirm_password = st.text_input("Confirm Password", type='password')
    
        if st.button("Register"):
            if password == confirm_password:
                st.success("User {} has been successfully registered.".format(username))
            else:
                st.error("Error: Passwords do not match")
    
    if __name__ == "__main__":
        main()
    >>>>>>> REPLACE
    ```
    
    This code will create a Streamlit application with a title "Registration Page". It will have four text input fields for the username, email, password, and password confirmation. The password fields will hide the input for security. There is also a "Register" button that, when clicked, will check if the entered passwords match. If they do, it will display a success message with the entered username. If they don't, it will display an error message.

---
+++ register.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def main():
+    st.title("Registration Page")
+    username = st.text_input("Username")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type='password')
+    confirm_password = st.text_input("Confirm Password", type='password')
+
+    if st.button("Register"):
+        if password == confirm_password:
+            st.success("User {} has been successfully registered.".format(username))
+        else:
+            st.error("Error: Passwords do not match")
+
+if __name__ == "__main__":
+    main()
