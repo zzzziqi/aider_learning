c594b8daee36bfbb4c6273a2bc7179d799032512
zzzziqi <zzeng002@gold.ac.uk>
2023-12-07 16:16:31 +0800

    aider: Replaced time.sleep(2) with a check for 2 seconds passing since the message was displayed before rerouting to the login page.
    # Aider chat conversation:
    
    USER: I wnat to dynamic sleep, when I show the message then count 2 second
    
    ASSISTANT: To implement a dynamic sleep, we can use the `time` module's `time()` function to get the current time and store it in `st.session_state`. Then, we can check if 2 seconds have passed since the message was displayed before rerouting to the login page.
    
    Here is the *SEARCH/REPLACE* block:
    
    register.py
    ```python
    <<<<<<< SEARCH
                    st.success("User {} has been successfully registered.".format(username))
                    time.sleep(2)
                    st.session_state.page = "Login"
                    st.experimental_rerun()
    =======
                    st.success("User {} has been successfully registered.".format(username))
                    st.session_state.register_time = time.time()
                    if 'register_time' in st.session_state and time.time() - st.session_state.register_time > 2:
                        st.session_state.page = "Login"
                        st.experimental_rerun()
    >>>>>>> REPLACE
    ```
    
    In the `REPLACE` block, I've replaced the `time.sleep(2)` line with a line that stores the current time in `st.session_state.register_time`. Then, I've added an `if` statement that checks if 2 seconds have passed since the message was displayed. If 2 seconds have passed, the script reroutes to the login page.

--- register.py
+++ register.py
@@ -18,9 +18,10 @@ def main():
 import time
 
                 st.success("User {} has been successfully registered.".format(username))
-                time.sleep(2)
-                st.session_state.page = "Login"
-                st.experimental_rerun()
+                st.session_state.register_time = time.time()
+                if 'register_time' in st.session_state and time.time() - st.session_state.register_time > 2:
+                    st.session_state.page = "Login"
+                    st.experimental_rerun()
         else:
             st.error("Error: Passwords do not match")
 
