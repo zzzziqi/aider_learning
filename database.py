d59e87941357047ecfe3830a24c53c4a04f32b77
zzzziqi <zzeng002@gold.ac.uk>
2023-12-07 15:12:03 +0800

    aider: Modified `create_connection()`, `add_user()`, and `user_exists()` functions to connect to `db.sqlite` instead of `aider/sqlite.db`.
    # Aider chat conversation:
    
    ASSISTANT: To create a database named `db`, you need to modify the `create_connection()` function in `database.py` to connect to `db.sqlite` instead of `aider/sqlite.db`.
    
    Here is the *SEARCH/REPLACE* block:
    
    database.py
    ```python
    <<<<<<< SEARCH
            conn = sqlite3.connect('aider/sqlite.db') # create a database connection to a SQLite database
    =======
            conn = sqlite3.connect('db.sqlite') # create a database connection to a SQLite database
    >>>>>>> REPLACE
    ```
    
    And you need to modify the `add_user()` and `user_exists()` functions to connect to `db.sqlite` as well.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    database.py
    ```python
    <<<<<<< SEARCH
        conn = sqlite3.connect('aider/sqlite.db')
    =======
        conn = sqlite3.connect('db.sqlite')
    >>>>>>> REPLACE
    ```
    
    database.py
    ```python
    <<<<<<< SEARCH
        conn = sqlite3.connect('aider/sqlite.db')
    =======
        conn = sqlite3.connect('db.sqlite')
    >>>>>>> REPLACE
    ```
    
    After these changes, when you call `create_connection()`, it will create a new SQLite database named `db.sqlite` if it doesn't exist. And the `add_user()` and `user_exists()` functions will interact with this database.

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlite3 import Error
 def create_connection():
     conn = None;
     try:
-        conn = sqlite3.connect('aider/sqlite.db') # create a database connection to a SQLite database
+        conn = sqlite3.connect('db.sqlite') # create a database connection to a SQLite database
         print(sqlite3.version)
     except Error as e:
         print(e)
@@ -13,14 +13,14 @@ def create_connection():
             conn.close()
 
 def add_user(username, password):
-    conn = sqlite3.connect('aider/sqlite.db')
+    conn = sqlite3.connect('db.sqlite')
     cur = conn.cursor()
     cur.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
     conn.commit()
     conn.close()
 
 def user_exists(username, password):
-    conn = sqlite3.connect('aider/sqlite.db')
+    conn = sqlite3.connect('db.sqlite')
     cur = conn.cursor()
     cur.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
     rows = cur.fetchall()
