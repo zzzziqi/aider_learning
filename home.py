d53a3a9a2518d686425d1c1141fced2aa18e481c
zzzziqi <zzeng002@gold.ac.uk>
2023-12-07 17:02:56 +0800

    Refactored home.py to include a welcome page and fix indentation issues.

--- home.py
+++ home.py
@@ -1,15 +1,17 @@
 import streamlit as st
 from login import main as login_main
 from register import main as register_main
+from welcome import main as welcome_main
 
 from database import create_connection, get_all_users, user_exists_by_name, add_user, get_user_email
+
 
 def main():
     create_connection()
     if "page" not in st.session_state:
         st.session_state.page = "Home"
     st.title("Home Page")
-    
+
     if st.sidebar.button("Home"):
         st.session_state.page = "Home"
         st.experimental_rerun()
@@ -19,7 +21,6 @@ def main():
     if st.sidebar.button("Register"):
         st.session_state.page = "Register"
         st.experimental_rerun()
-
 
     if st.session_state.page == "Home":
         st.subheader("Home")
@@ -33,15 +34,15 @@ def main():
             st.write(f"Username: {user[0]}, Email: {user[1]}, Password: {user[2]}")
     elif st.session_state.page == "Login":
         login_main()
-from welcome import main as welcome_main
 
-elif st.session_state.page == "Welcome":
-    welcome_main()
+    elif st.session_state.page == "Welcome":
+        welcome_main()
     elif st.session_state.page == "Register":
         register_main()
 
     if not user_exists_by_name("zzq"):
         add_user("zzq", "zzq@qq.com", "qqz")
+
 
 if __name__ == "__main__":
     main()
